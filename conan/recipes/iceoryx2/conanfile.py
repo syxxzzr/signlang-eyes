a50522aec2e0808a3e443f0a8736835a342cdf33
syxxzzr <syxxzzr@gmail.com>
2026-07-01 12:38:07 +0800

    refactor: update iceoryx2 conan recipe

--- conan/recipes/iceoryx2/conanfile.py
+++ conan/recipes/iceoryx2/conanfile.py
@@ -1,28 +1,92 @@
 from conan import ConanFile
 from conan.errors import ConanInvalidConfiguration
-from conan.tools.files import copy
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
+from conan.tools.files import get, save
 from pathlib import Path
 
 
 class Iceoryx2Conan(ConanFile):
     name = "iceoryx2"
-    version = "0.9.999"
     package_type = "shared-library"
-    settings = "os", "arch"
-    exports_sources = "vendor/*"
+    settings = "os", "arch", "compiler", "build_type"
+
+    def set_version(self):
+        self.version = self.conan_data["version"]
+
+    def _source_data(self):
+        return self.conan_data["sources"][str(self.version)]
+
+    def _rust_target_triplet(self):
+        if str(self.settings.arch) == "armv8":
+            return "aarch64-unknown-linux-gnu"
+        if str(self.settings.arch) in ("x86_64", "amd64"):
+            return ""
+        raise ConanInvalidConfiguration(f"Unsupported iceoryx2 Rust target for arch={self.settings.arch}")
+
+    def _target_env_name(self, rust_target_triplet):
+        return rust_target_triplet.upper().replace("-", "_")
 
     def validate(self):
-        if str(self.settings.os) != "Linux" or str(self.settings.arch) != "armv8":
-            raise ConanInvalidConfiguration("This prebuilt iceoryx2 package is only available for Linux armv8")
+        if str(self.settings.os) != "Linux":
+            raise ConanInvalidConfiguration("This iceoryx2 recipe is only configured for Linux")
+
+    def layout(self):
+        cmake_layout(self)
+
+    def source(self):
+        source_data = self._source_data()
+        get(self, url=source_data["url"], sha256=source_data["sha256"], strip_root=True)
+
+    def generate(self):
+        rust_target_triplet = self._rust_target_triplet()
+
+        tc = CMakeToolchain(self)
+        tc.variables["BUILD_CXX"] = True
+        tc.variables["BUILD_EXAMPLES"] = False
+        tc.variables["BUILD_TESTING"] = False
+        if rust_target_triplet:
+            tc.variables["RUST_TARGET_TRIPLET"] = rust_target_triplet
+        tc.generate()
+
+        if rust_target_triplet:
+            self._generate_cargo_config(rust_target_triplet)
+
+    def _generate_cargo_config(self, rust_target_triplet):
+        compiler_executables = self.conf.get("tools.build:compiler_executables", default={})
+        c_compiler = compiler_executables.get("c")
+        if not c_compiler:
+            raise ConanInvalidConfiguration(
+                "Cross compiling iceoryx2 requires tools.build:compiler_executables['c'] for Cargo"
+            )
+
+        sysroot = self.conf.get("tools.build:sysroot", default="")
+        target_env_name = self._target_env_name(rust_target_triplet)
+        cargo_config = [
+            "[build]",
+            'rustdocflags = ["-D", "warnings"]',
+            "",
+            "[env]",
+            'RUST_TEST_THREADS = "1"',
+            'RUST_BACKTRACE = "1"',
+            f'CC_{target_env_name} = "{c_compiler}"',
+            f'CARGO_TARGET_{target_env_name}_LINKER = "{c_compiler}"',
+            "",
+            f"[target.{rust_target_triplet}]",
+            f'linker = "{c_compiler}"',
+        ]
+        if sysroot:
+            cargo_config.append(f'rustflags = ["-C", "link-arg=--sysroot={sysroot}"]')
+
+        save(self, Path(self.source_folder) / ".cargo" / "config.toml", "\n".join(cargo_config) + "\n")
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
 
     def package(self):
-        source_root = Path(self.source_folder) / "vendor"
-        if not source_root.exists():
-            raise ConanInvalidConfiguration(f"iceoryx2 prebuilt root not found: {source_root}")
-
-        copy(self, "*", src=source_root / "include", dst=Path(self.package_folder) / "include")
-        copy(self, "*", src=source_root / "lib", dst=Path(self.package_folder) / "lib")
-        copy(self, "*", src=source_root / "share", dst=Path(self.package_folder) / "share")
+        cmake = CMake(self)
+        cmake.install()
 
     def package_info(self):
         lib_dir = Path(self.package_folder) / "lib"
@@ -31,7 +95,7 @@ class Iceoryx2Conan(ConanFile):
         self.cpp_info.set_property("cmake_file_name", "iceoryx2-cxx")
         self.cpp_info.set_property("cmake_target_name", "iceoryx2-cxx::shared-lib-cxx")
         self.cpp_info.bindirs = []
-        self.cpp_info.includedirs = ["include/iceoryx2/v0.9.999"]
+        self.cpp_info.includedirs = [f"include/iceoryx2/v{self.version}"]
         self.cpp_info.libdirs = ["lib"]
         self.cpp_info.libs = ["iceoryx2_cxx", "iceoryx2_ffi_c"]
         self.cpp_info.system_libs = ["dl", "rt", "m", "pthread"]
